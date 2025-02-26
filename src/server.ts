87b5f13cf6d2d6c1941ac91cbf150cf01b33caad
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-26 19:37:01 -0300

    added subscribe ranking position route

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import { env } from './env'
 import { accessInviteLinkRoute } from './routes/access-invite-link-route'
 import { getSubscriberInviteClickRoute } from './routes/get-subscriber-invite-clicks-route'
 import { getSubscriberInviteCountRoute } from './routes/get-subscriber-invite-count-route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -39,6 +40,7 @@ app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClickRoute)
 app.register(getSubscriberInviteCountRoute)
+app.register(getSubscriberRankingPositionRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running')
