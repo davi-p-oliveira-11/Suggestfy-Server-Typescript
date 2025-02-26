d5c740169720d9c98bf34964239a76497e1b15b5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-26 19:16:22 -0300

    addded subscriber invite to route ts

--- src/functions/get-subscriber-invites-count.ts
+++ src/functions/get-subscriber-invites-count.ts
@@ -7,7 +7,7 @@ interface GetSubscriberInvitesCountParams {
 export async function getSubscribeInviteCount({
   subscriberId,
 }: GetSubscriberInvitesCountParams) {
-  const count = await redis.zscore('referral:access-count', subscriberId)
+  const count = await redis.zscore('referral:ranking', subscriberId)
 
   return { count: count ? Number.parseInt(count) : 0 }
 }
