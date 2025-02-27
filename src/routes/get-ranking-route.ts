5d2cb4d27ab8e603e1d5c4ef63f25c4b152ceacc
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-26 21:32:17 -0300

    ended development before production

--- src/routes/get-ranking-route.ts
+++ src/routes/get-ranking-route.ts
@@ -2,25 +2,30 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { getRanking } from '../functions/get-ranking'
 
-export const getSubscriberInviteCountRoute: FastifyPluginAsyncZod =
-  async app => {
-    app.get(
-      '/ranking',
-      {
-        schema: {
-          summary: 'Get ranking',
-          tags: ['referral'],
-          /* response: {
-            200: z.object({
-              count: z.number(),
-            }),
-          }, */
+export const getRankingRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/ranking',
+    {
+      schema: {
+        summary: 'Get ranking',
+        tags: ['referral'],
+        response: {
+          200: z.object({
+            ranking: z.array(
+              z.object({
+                id: z.string(),
+                name: z.string(),
+                score: z.number(),
+              })
+            ),
+          }),
         },
       },
-      async request => {
-        await getRanking()
+    },
+    async request => {
+      const { rankingWithScore } = await getRanking()
 
-        return 'ok'
-      }
-    )
-  }
+      return { ranking: rankingWithScore }
+    }
+  )
+}
